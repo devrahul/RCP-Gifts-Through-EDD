0ae15423cde8014cd8260521b82f0f8aa5968f79
Bearded Avenger <email@nickhaskins.com>
2014-02-05 10:01:30 -0600

    i18n

--- includes/class-gifts.php
+++ includes/class-gifts.php
@@ -7,7 +7,7 @@ class RCP_Gift_Products {
 	}
 
 	public function menu_page(){
-		add_submenu_page( 'rcp-members', __( 'Gifts', 'rcp' ), __( 'Gifts', 'rcp' ),'manage_options', 'rcp-gifts', array($this,'draw_page') );
+		add_submenu_page( 'rcp-members', __( 'Gifts', 'rcp-gifts' ), __( 'Gifts', 'rcp-gifts' ),'manage_options', 'rcp-gifts', array($this,'draw_page') );
 	}
 
 	public function draw_page(){
@@ -26,39 +26,39 @@ class RCP_Gift_Products {
 
 
 			?>
-			<h2><?php _e( 'RCP Gifts', 'rcp' ); ?></h2>
+			<h2><?php _e( 'RCP Gifts', 'rcp-gifts' ); ?></h2>
 
 			<table class="wp-list-table widefat fixed posts">
 				<thead>
 					<tr>
-						<th class="rcp-discounts-id-col"><?php _e( 'ID', 'rcp' ); ?></th>
-						<th class="rcp-discounts-name-col" ><?php _e( 'Name', 'rcp' ); ?></th>
-						<th class="rcp-discounts-desc-col"><?php _e( 'Description', 'rcp' ); ?></th>
-						<th class="rcp-discounts-code-col" ><?php _e( 'Code', 'rcp' ); ?></th>
-						<th class="rcp-discounts-subscription-col" ><?php _e( 'Subscription', 'rcp' ); ?></th>
-						<th class="rcp-discounts-amount-col"><?php _e( 'Amount', 'rcp' ); ?></th>
-						<th class="rcp-discounts-type-col"><?php _e( 'Type', 'rcp' ); ?></th>
-						<th class="rcp-discounts-status-col"><?php _e( 'Status', 'rcp' ); ?></th>
-						<th class="rcp-discounts-uses-col"><?php _e( 'Uses', 'rcp' ); ?></th>
-						<th class="rcp-discounts-uses-left-col"><?php _e( 'Uses Left', 'rcp' ); ?></th>
-						<th class="rcp-discounts-expir-col" ><?php _e( 'Expiration', 'rcp' ); ?></th>
-						<th class="rcp-discounts-actions-col" ><?php _e( 'Actions', 'rcp' ); ?></th>
+						<th class="rcp-discounts-id-col"><?php _e( 'ID', 'rcp-gifts' ); ?></th>
+						<th class="rcp-discounts-name-col" ><?php _e( 'Name', 'rcp-gifts' ); ?></th>
+						<th class="rcp-discounts-desc-col"><?php _e( 'Description', 'rcp-gifts' ); ?></th>
+						<th class="rcp-discounts-code-col" ><?php _e( 'Code', 'rcp-gifts' ); ?></th>
+						<th class="rcp-discounts-subscription-col" ><?php _e( 'Subscription', 'rcp-gifts' ); ?></th>
+						<th class="rcp-discounts-amount-col"><?php _e( 'Amount', 'rcp-gifts' ); ?></th>
+						<th class="rcp-discounts-type-col"><?php _e( 'Type', 'rcp-gifts' ); ?></th>
+						<th class="rcp-discounts-status-col"><?php _e( 'Status', 'rcp-gifts' ); ?></th>
+						<th class="rcp-discounts-uses-col"><?php _e( 'Uses', 'rcp-gifts' ); ?></th>
+						<th class="rcp-discounts-uses-left-col"><?php _e( 'Uses Left', 'rcp-gifts' ); ?></th>
+						<th class="rcp-discounts-expir-col" ><?php _e( 'Expiration', 'rcp-gifts' ); ?></th>
+						<th class="rcp-discounts-actions-col" ><?php _e( 'Actions', 'rcp-gifts' ); ?></th>
 					</tr>
 				</thead>
 				<tfoot>
 					<tr>
-						<th><?php _e( 'ID', 'rcp' ); ?></th>
-						<th><?php _e( 'Name', 'rcp' ); ?></th>
-						<th><?php _e( 'Description', 'rcp' ); ?></th>
-						<th><?php _e( 'Code', 'rcp' ); ?></th>
-						<th><?php _e( 'Subscription', 'rcp' ); ?></th>
-						<th><?php _e( 'Amount', 'rcp' ); ?></th>
-						<th><?php _e( 'Type', 'rcp' ); ?></th>
-						<th><?php _e( 'Status', 'rcp' ); ?></th>
-						<th><?php _e( 'Uses', 'rcp' ); ?></th>
-						<th><?php _e( 'Uses Left', 'rcp' ); ?></th>
-						<th><?php _e( 'Expiration', 'rcp' ); ?></th>
-						<th><?php _e( 'Actions', 'rcp' ); ?></th>
+						<th><?php _e( 'ID', 'rcp-gifts' ); ?></th>
+						<th><?php _e( 'Name', 'rcp-gifts' ); ?></th>
+						<th><?php _e( 'Description', 'rcp-gifts' ); ?></th>
+						<th><?php _e( 'Code', 'rcp-gifts' ); ?></th>
+						<th><?php _e( 'Subscription', 'rcp-gifts' ); ?></th>
+						<th><?php _e( 'Amount', 'rcp-gifts' ); ?></th>
+						<th><?php _e( 'Type', 'rcp-gifts' ); ?></th>
+						<th><?php _e( 'Status', 'rcp-gifts' ); ?></th>
+						<th><?php _e( 'Uses', 'rcp-gifts' ); ?></th>
+						<th><?php _e( 'Uses Left', 'rcp-gifts' ); ?></th>
+						<th><?php _e( 'Expiration', 'rcp-gifts' ); ?></th>
+						<th><?php _e( 'Actions', 'rcp-gifts' ); ?></th>
 					</tr>
 				</tfoot>
 				<tbody>
@@ -77,39 +77,39 @@ class RCP_Gift_Products {
 								if ( $discount->subscription_id > 0 ) {
 									echo rcp_get_subscription_name( $discount->subscription_id );
 								} else {
-									echo __( 'All Levels', 'rcp' );
+									echo __( 'All Levels', 'rcp-gifts' );
 								}
 								?>
 							</td>
 							<td><?php echo rcp_discount_sign_filter( $discount->amount, $discount->unit ); ?></td>
-							<td><?php echo $discount->unit == '%' ? __( 'Percentage', 'rcp' ) : __( 'Flat', 'rcp' ); ?></td>
+							<td><?php echo $discount->unit == '%' ? __( 'Percentage', 'rcp-gifts' ) : __( 'Flat', 'rcp-gifts' ); ?></td>
 							<td>
 								<?php
 									if(rcp_is_discount_not_expired( $discount->id ) ) {
-										echo rcp_get_discount_status( $discount->id ) == 'active' ? __( 'active', 'rcp' ) : __( 'disabled', 'rcp' );
+										echo rcp_get_discount_status( $discount->id ) == 'active' ? __( 'active', 'rcp-gifts' ) : __( 'disabled', 'rcp-gifts' );
 									} else {
-										_e( 'expired', 'rcp' );
+										_e( 'expired', 'rcp-gifts' );
 									}
 								?>
 							</td>
 							<td><?php if( $discount->max_uses > 0 ) { echo rcp_count_discount_code_uses( $discount->code ) . '/' . $discount->max_uses; } else { echo rcp_count_discount_code_uses( $discount->code ); }?></td>
 							<td><?php echo rcp_discount_has_uses_left( $discount->id ) ? 'yes' : 'no'; ?></td>
-							<td><?php echo $discount->expiration == '' ? __( 'none', 'rcp' ) : date_i18n( 'Y-m-d', strtotime( $discount->expiration ) ); ?></td>
+							<td><?php echo $discount->expiration == '' ? __( 'none', 'rcp-gifts' ) : date_i18n( 'Y-m-d', strtotime( $discount->expiration ) ); ?></td>
 							<?php do_action('rcp_discounts_page_table_column', $discount->id); ?>
 							<td>
 								<?php if(rcp_get_discount_status($discount->id) == 'active') { ?>
-									<a href="<?php echo add_query_arg( 'deactivate_discount', $discount->id, $page ); ?>"><?php _e( 'Deactivate', 'rcp' ); ?></a> |
+									<a href="<?php echo add_query_arg( 'deactivate_discount', $discount->id, $page ); ?>"><?php _e( 'Deactivate', 'rcp-gifts' ); ?></a> |
 								<?php } else { ?>
-									<a href="<?php echo add_query_arg( 'activate_discount', $discount->id, $page ); ?>"><?php _e( 'Activate', 'rcp' ); ?></a> |
+									<a href="<?php echo add_query_arg( 'activate_discount', $discount->id, $page ); ?>"><?php _e( 'Activate', 'rcp-gifts' ); ?></a> |
 								<?php } ?>
-								<a href="<?php echo add_query_arg( 'delete_discount', $discount->id, $page ); ?>" class="rcp_delete_discount"><?php _e( 'Delete', 'rcp' ); ?></a>
+								<a href="<?php echo add_query_arg( 'delete_discount', $discount->id, $page ); ?>" class="rcp_delete_discount"><?php _e( 'Delete', 'rcp-gifts' ); ?></a>
 							</td>
 						</tr>
 					<?php
 					$i++;
 					endforeach;
 				else : ?>
-				<tr><td colspan="11"><?php _e( 'No discount codes added yet.', 'rcp' ); ?></td>
+				<tr><td colspan="11"><?php _e( 'No discount codes added yet.', 'rcp-gifts' ); ?></td>
 				<?php endif; ?>
 			</table>
 		</div>
